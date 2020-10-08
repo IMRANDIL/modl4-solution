7bf72ff24b4bcef443b5fe154e935a4aac8c8919
Ali <aliimranadil2@gmail.com>
2020-10-08 15:57:18 +0530

    coursera-commit

---
+++ SpeakGoodby.js
@@ -0,0 +1,15 @@
+
+
+(function(window) {
+    var byeSpeaker = {};
+    byeSpeaker.speak = "Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim";
+    var speakWord = "Good Bye";
+   byeSpeaker.speak = function (name) {
+       console.log(speakWord+ " " + name);
+   }
+       
+        
+    
+    window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ SpeakHelllo.js
@@ -0,0 +1,14 @@
+
+
+(function(window) {
+var helloSpeaker = {};
+helloSpeaker.speak = "Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim";
+var speakWord = "Hello";
+helloSpeaker.speak = function speak (name) {
+    console.log(speakWord+ " " + name);
+}
+
+window.helloSpeaker = helloSpeaker;
+ 
+
+})(window);
